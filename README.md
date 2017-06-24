880f46930258b1efe322bcb2460d63a65b3f626b
mixassio <mixail.bondarev@gmail.com>
2017-06-24 17:34:48 +0300

    sent for first check

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+The database devman stores information about who and when sent the task for verification.
+You can obtain it using the API: 'http://devman.org/api/challenges/solution_attempts/?page=2'
+The script displays users who sent tasks for review after 24:00.
+To run the script requires an interpreter python 3
+
+```#!bash
+
+$ python3 seek_dev_nighters.py
+id384163500
+desolator27
+id20889227
+mindhatter
+nongames
+reatlio79
+galbator1x
+id164029976
+ilyap
+pspiskunov
+chadarr
+СергейАнатольевич
+МихаилБондарев
+id219529777
+alexander.i.kamenev
+jekov
+ДмитрийВоронов
+id15856034
+#
+```
+
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,37 @@
+import requests
+from pytz import timezone
+from datetime import datetime
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
+    devman_api = requests.get('https://devman.org/api/challenges/solution_attempts/?page=1')
+    devman_content = devman_api.json()
+    pages = int(devman_content['number_of_pages'])
+    for page in range(1, pages+1):
+        devman_api = requests.get('https://devman.org/api/challenges/solution_attempts/?page={}'.format(page))
+        devman_content = devman_api.json()
+        for list_users in devman_content['records']:
+            yield {
+            'username': list_users['username'],
+            'timestamp': list_users['timestamp'],
+            'timezone': list_users['timezone']
         }
 
-def get_midnighters():
-    pass
+
+def get_midnighters(user_info):
+    tz = user_info['timezone']
+    if user_info['timestamp'] is not None:
+        server_time = datetime.fromtimestamp(user_info['timestamp'])
+        client_time = timezone(tz).fromutc(server_time)
+        if client_time.hour < 5:
+            return True
+
 
 if __name__ == '__main__':
-  pass
+    list_users = load_attempts()
+    set_midnighters = set()
+    for user_info in list_users:
+        if get_midnighters(user_info):
+            set_midnighters.add(user_info['username'])
+    for user in set_midnighters:
+        print(user)
